9ca606a7ff110340de49b12ccacda6c8f410042c
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2020-02-01 14:36:05 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,53 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do
+    redirect '/recipes'
+  end
+
+  get '/recipes' do
+    #shows all recipes
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    #show form to create new
+    erb :new
+  end
+
+  post '/recipes' do
+    #create new recipe
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do 
+    #displays recipe based on id
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    #displays form to edit based on id
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    #updates recipe based on id
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect "/recipes"
+  end
 
 end
